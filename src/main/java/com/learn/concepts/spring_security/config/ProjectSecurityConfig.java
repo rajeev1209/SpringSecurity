046693a89c6027b0b00e497f55d64c3b0dae8dbf
rajeev1209 <kumar.rajeevranjan10@yahoo.com>
2024-10-27 12:35:09 -0400

    Section 3 - added UserDetailsService, PasswordEncoder, and CompromisedPasswordChecker

--- src/main/java/com/learn/concepts/spring_security/config/ProjectSecurityConfig.java
+++ src/main/java/com/learn/concepts/spring_security/config/ProjectSecurityConfig.java
@@ -2,9 +2,17 @@ package com.learn.concepts.spring_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -14,11 +22,29 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/myAccount", "/myCards", "myBalance", "myLoans").authenticated()
-                .requestMatchers("/notices", "/contact","/error").permitAll());
+                .requestMatchers("/notices", "/contact", "/error").permitAll());
                 /*.formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable);*/
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}Eazy@12345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$MaU9eJc51Jl5VVRmvqjazOiaVWEklS4JuwqnPxJNuxlVmnIC5NjOO")
+                .authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
